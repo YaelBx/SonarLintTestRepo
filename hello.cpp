d3182de122349e270650b9cdb33d16d298ef5292
Yael Boutreux <yael.boutreux@cp.com>
2024-05-30 10:53:45 +0200

    A little code modification to break the rule cpp/RSPEC-999/, that SonarLint should catch.
    Signed-off-by: Yael Boutreux <yael.boutreux@cp.com>

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include "hello.h"
 
-void hello(){
-	std::cout << "Hello World!\n";
+int hello() {
+  std::cout << "Hello SonarLint!\n";
+
+  /* A simple broken rule that Sonar should detect
+   * "goto" should jump to labels declared later in the same function
+   *  https://rules.sonarsource.com/cpp/RSPEC-999/
+   */
+  volatile int j = 0;
+L1:
+  ++j;
+  if (10 == j) {
+    goto L2; // forward jump ignored
+  }
+  // ...
+  goto L1; // Noncompliant
+L2:
+  return ++j;
 }
